5db77257a6f4420df0e61da28720f707d4b586c0
Augusto dos Anjos Almeida <augusto.almeida@gmail.com>
2016-11-03 10:06:21 -0200

    Inclusão de alto contraste.

--- js/cnir.js
+++ js/cnir.js
@@ -1,1 +1,9 @@
+// habilitar alto contraste
+$('#alto-contraste').click(function (e) {
+	e.preventDefault();
+	
+	$(this).parents('body').toggleClass('contraste');
+});
+
+// habilitar popover
 $('[data-toggle="popover"]').popover();
