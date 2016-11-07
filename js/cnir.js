fcff4b097fe91dde1f62f8b28ad315e6e6baca27
Augusto dos Anjos Almeida <augusto.almeida@gmail.com>
2016-11-07 10:27:17 -0200

    Inclusão do menu de navegação.

--- js/cnir.js
+++ js/cnir.js
@@ -7,3 +7,27 @@ $('#alto-contraste').click(function (e) {
 
 // habilitar popover
 $('[data-toggle="popover"]').popover();
+
+// menu
+// a cache to collapsed itens
+var collpaseds = {};
+
+$('#menu [data-toggle="collapse"]')
+.append('<span class="fa fa-chevron-down pull-right" aria-hidden="true"></span>')
+.click(function(event) {
+    event.preventDefault();
+    event.stopPropagation();
+
+    $(this).toggleClass('open');
+
+    var idCollapase = $(this).data('target');
+    if (!collpaseds[idCollapase]) {
+        collpaseds[idCollapase] = $(idCollapase);
+    }
+
+    if (collpaseds[idCollapase]) {
+        collpaseds[idCollapase].collapse('toggle');
+    } else {
+        console.log('Element (id) not found: ', idCollapase);
+    }
+});
